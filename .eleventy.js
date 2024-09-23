a26f2aef92b5af1d5883522b96ba984770417b0e
Todd L. Schneckloth <todds@rhinopack.com>
2024-09-22 20:53:23 -0600

    feat(mod5): Final Project WIP

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,13 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy(".nojekyll");
 
     return {
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        templateFormats: ["html", "md", "njk"],
         dir: {
             input: "src",
             output: "docs",
+            layouts: "_includes/layouts",
         },
     };
 };
